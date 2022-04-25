04dd11aef44bbf5d5b869337b485c69409893b60
lilei <13186102535@163.com>
2022-04-25 09:21:47 +0800

    Merge branch 'master' of /Users/futurelwy/MyIdeaProjects with conflicts.

--- design-mode/src/main/java/com/future/strategy_pattern/StimulateDuck.java
+++ design-mode/src/main/java/com/future/strategy_pattern/StimulateDuck.java
@@ -1,1 +1,1 @@
-package com.future.strategy_pattern;/** * @Description: * @Author: lilei58 * @Date: Created in 2022/4/25 上午9:14 */public class StimulateDuck {    public static void main(String[] args) {    }}
+package com.future.strategy_pattern;/** * @Description: 策略模式 * @Author: lilei58 * @Date: Created in 2022/4/25 上午9:14 */public class StimulateDuck {    public static void main(String[] args) {        System.out.println("策略模式");    }}
