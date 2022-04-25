cabe931e8f25d05b9a3a5c9e8e07a1ba182be44e
lilei <13186102535@163.com>
2022-04-25 09:23:58 +0800

    master 设计模式

--- design-mode/src/main/java/com/future/strategy_pattern/StimulateDuck.java
+++ design-mode/src/main/java/com/future/strategy_pattern/StimulateDuck.java
@@ -1,1 +1,13 @@
-package com.future.strategy_pattern;/** * @Description: TODO 策略模式 * @Author: lilei58 * @Date: Created in 2022/4/25 上午9:14 */public class StimulateDuck {    public static void main(String[] args) {        System.out.println("策略模式");    }}
+package com.future.strategy_pattern;
+
+/**
+ * @Description: TODO 策略模式 1
+ * @Author: lilei58
+ * @Date: Created in 2022/4/25 上午9:14
+ */
+public class StimulateDuck {
+
+    public static void main(String[] args) {
+        System.out.println("策略模式");
+    }
+}
