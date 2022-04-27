aac30142a47efebf26b20d10c4d15c9554df39d0
lilei <13186102535@163.com>
2022-04-27 09:54:04 +0800

    master 设计模式

--- design-mode/src/main/java/com/future/strategy_pattern/duck/GreenHeadDuck.java
+++ design-mode/src/main/java/com/future/strategy_pattern/duck/GreenHeadDuck.java
@@ -1,1 +1,23 @@
-package com.future.strategy_pattern.duck;import com.future.strategy_pattern.behavior.flybehavior.GoodFlyBehavior;import com.future.strategy_pattern.behavior.quackbehavior.GaGaQuackBehavior;/** * @Description: * @Author: lilei58 * @Date: Created in 2022/4/27 上午8:48 */public class GreenHeadDuck extends Duck {    public GreenHeadDuck() {        mFlyBehavior = new GoodFlyBehavior();        mQuackBehavior = new GaGaQuackBehavior();    }    @Override    public void display() {        System.out.println("**GreenHead**");    }}
+package com.future.strategy_pattern.duck;
+
+import com.future.strategy_pattern.behavior.flybehavior.GoodFlyBehavior;
+import com.future.strategy_pattern.behavior.quackbehavior.GaGaQuackBehavior;
+
+/**
+ * @Description:
+ * @Author: lilei58
+ * @Date: Created in 2022/4/27 上午8:48
+ */
+public class GreenHeadDuck extends Duck {
+
+    public GreenHeadDuck() {
+        mFlyBehavior = new GoodFlyBehavior();
+        mQuackBehavior = new GaGaQuackBehavior();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("**GreenHead**");
+    }
+
+}
